3348f0d7f9fc2aabf8beaac02a489b6354ac665b
mahemall <free.ibm@gmail.com>
2022-05-07 17:10:59 +0530

    Initial commit

---
+++ precache-manifest.232f1609d05a158cf8c9f0956ca77f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb121fd1bb50ecdf82abf26ac6343bf",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "a094f3b24c39f277664d",
+    "url": "/amazon-clone/static/css/main.79ba9d6e.chunk.css"
+  },
+  {
+    "revision": "ddacdc929ec95a44f2dc",
+    "url": "/amazon-clone/static/js/2.3d94fb48.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/amazon-clone/static/js/2.3d94fb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a094f3b24c39f277664d",
+    "url": "/amazon-clone/static/js/main.d0b84924.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
